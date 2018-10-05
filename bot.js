7f68008eabc55ffadc3bc80a1a46ac2442bc70c7
A <anchl@users.noreply.github.com>
2018-10-05 14:08:05 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -77,7 +77,7 @@ client.on('message', message => {
 		outcome = "<:aqua1:497870765627015179>**LOSER!**<:mim1:497870765962559498>"; 
 	}
            
-		message.channel.send('<:2ba:497731536649584650> WEEB SLOTS <:2ba:497731536649584650>\n-----------------\n' + x[1] + ' : ' + x[2] + ' : ' + x[3] + '\n  \n' + x[4] + ' : ' + x[5] + ' : ' + x[6] + ' ◀ Get 3 in a row here to win! \n \n' + x[7] + ' : ' + x[8] + ' : ' + x[0] + '\n-----------------\n' +  outcome);       
+		message.channel.send('<:2ba:497731536649584650>***WEEBS***<:2ba:497731536649584650>\n-----------------\n' + x[1] + ' : ' + x[2] + ' : ' + x[3] + '\n  \n' + x[4] + ' : ' + x[5] + ' : ' + x[6] + ' ◀ Get 3 in a row here to win! \n \n' + x[7] + ' : ' + x[8] + ' : ' + x[0] + '\n-----------------\n' +  outcome);       
     	
 	    	// command cooldown - https://anidiotsguide_old.gitbooks.io/discord-js-bot-guide/content/examples/misc-examples.html
 		talkedRecently.add(message.author.id);
