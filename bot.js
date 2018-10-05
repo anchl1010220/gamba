7ee400ae2d87af2001e6d59a3fae0ff5c71d926f
A <anchl@users.noreply.github.com>
2018-10-05 03:38:39 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ client.on('message', message => {
         // const y = client.emojis.find("name", "ayayay");   
     	message.reply('SEND HELP ~ TRY !weeb');
   	}
-    if (message.content === 'AYAYAY') {
+    if (message.content === ':AYAYAY:') {
         message.react("497706450458509312")
     }
 });
