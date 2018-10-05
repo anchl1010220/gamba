31f4c2783d3b3353cb2a2c338045e490cb24f13e
A <anchl@users.noreply.github.com>
2018-10-05 14:04:49 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -74,10 +74,10 @@ client.on('message', message => {
 	if (x[4] === x[5] && x[5] === x[6]) {
  	 	outcome = "<:lala2:497870765937655828> YOU WIN! <:lala1:497870766067417088> ";
 	} else {
-		outcome = "<:mim1:497870765962559498> YOU LOSE! <:aqua1:497870765627015179>"; 
+		outcome = "<:aqua1:497870765627015179> YOU LOSE! <:mim1:497870765962559498>"; 
 	}
            
-		message.channel.send('<:2ba:497731536649584650> WEEB SLOTS <:2ba:497731536649584650>\n------------------\n' + x[1] + ' : ' + x[2] + ' : ' + x[3] + '\n  \n' + x[4] + ' : ' + x[5] + ' : ' + x[6] + ' ◀ Get 3 in a row here to win! \n \n' + x[7] + ' : ' + x[8] + ' : ' + x[0] + '\n------------------\n' +  outcome);       
+		message.channel.send('<:2ba:497731536649584650> WEEB SLOTS <:2ba:497731536649584650>\n-----------------\n' + x[1] + ' : ' + x[2] + ' : ' + x[3] + '\n  \n' + x[4] + ' : ' + x[5] + ' : ' + x[6] + ' ◀ Get 3 in a row here to win! \n \n' + x[7] + ' : ' + x[8] + ' : ' + x[0] + '\n-----------------\n' +  outcome);       
     	
 	    	// command cooldown - https://anidiotsguide_old.gitbooks.io/discord-js-bot-guide/content/examples/misc-examples.html
 		talkedRecently.add(message.author.id);
