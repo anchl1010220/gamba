f880498114c5a585167ba785eb9b2e3757cbaaa4
A <anchl@users.noreply.github.com>
2018-10-08 15:06:22 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,22 +35,7 @@ client.on("guildDelete", guild => {
 client.on('message', message => {
 	if(message.author.bot) return;
 
-	  if(command === "ping") {  // https://gist.github.com/eslachance/3349734a98d30011bb202f47342601d3
-    	// Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-   	 // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
-   	 const m = await message.channel.send("Ping?");
-   	 m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
- 	 }
 
-	  if(command === "say") {
-  		  // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
-  		  // To get the "message" itself we join the `args` back into a string with spaces: 
- 		   const sayMessage = args.join(" ");
-  		  // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
- 		   message.delete().catch(O_o=>{}); 
- 		   // And we get the bot to say the thing: 
-  		  message.channel.send(sayMessage);
-	  }
 
     if (responseObject[message.content]) {
         message.channel.send(responseObject[message.content]); // Line 6, check responseObject
