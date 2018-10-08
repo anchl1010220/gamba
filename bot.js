e0643123680e380ab7e2b90c0195f1d38defa284
A <anchl@users.noreply.github.com>
2018-10-08 04:37:28 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -116,11 +116,11 @@ client.on('message', message => {
 		outcome = "<:aqua1:497870765627015179>**LOSER!**<:mim1:497870765962559498>"; 
 	}
            
-		message.channel.send('<:2ba:497731536649584650>***WEEBS***<:2ba:497731536649584650>\n-----------------\n' + x[1] + ' : ' + x[2] + ' : ' + x[3] + ' - ◀\n  \n' + x[4] + ' : ' + x[5] + ' : ' + x[6] + ' - ◀ Match 3 in a row to win! \n \n' + x[7] + ' : ' + x[8] + ' : ' + x[0] + ' - ◀\n-----------------\n' +  outcome);       
+		message.channel.send('<:2ba:497731536649584650>***WEEBS***<:2ba:497731536649584650>\n-----------------\n' + x[1] + ' : ' + x[2] + ' : ' + x[3] + ' - ◀\n  \n' + x[4] + ' : ' + x[5] + ' : ' + x[6] + ' - ◀ Match 3 in any row to win! \n \n' + x[7] + ' : ' + x[8] + ' : ' + x[0] + ' - ◀\n-----------------\n' +  outcome);       
     	
 	    	// command cooldown - https://anidiotsguide_old.gitbooks.io/discord-js-bot-guide/content/examples/misc-examples.html
 		talkedRecently.add(message.author.id);
-		setTimeout(() => { talkedRecently.delete(message.author.id); }, 50); // 5 sec cd (5000ms)
+		setTimeout(() => { talkedRecently.delete(message.author.id); }, 5000); // 5 sec cd (5000ms)
     	}
 });
 
