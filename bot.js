bf7cfa0e6f5df71051931bea1f4abc05cbd1aa9c
A <anchl@users.noreply.github.com>
2018-10-08 15:34:20 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,6 +34,14 @@ client.on("guildDelete", guild => {
 
 client.on('message', message => {
 	if(message.author.bot) return;
+	
+		  if(message.content === 'ping') {  // https://gist.github.com/eslachance/3349734a98d30011bb202f47342601d3
+    	// Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+   	 // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+   	 const m = await message.channel.send("Ping?");
+   	 m.edit('Pong! Latency is ' + (m.createdTimestamp - message.createdTimestamp) + 'ms. API Latency is ' + Math.round(client.ping) + 'ms');
+ 	 }
+
 
     if (responseObject[message.content]) {
         message.channel.send(responseObject[message.content]); // Line 6, check responseObject
