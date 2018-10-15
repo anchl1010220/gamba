edf9a7df6d9021901e97660487184c9c2f9d0be2
A <anchl@users.noreply.github.com>
2018-10-14 18:14:10 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -146,7 +146,7 @@ client.on('message', message => {
 	{
 		// lol
 		const channel = message.channel;
-		message.channel.send("```css\n📋 Rank | Name\n\n[1]     > <@" + message.author.id + ">\n            Total Score: 42069\n\n\“Unless you live life for yourself, you won’t even know what happiness is.\”\n- Tosaka Rin, Unlimited Blade Works\n```");
+		message.channel.send("```css\n📋 Rank | Name\n\n[1]     > #" + message.author.toString() + "\n            Total Score: 42069\n\n\“Unless you live life for yourself, you won’t even know what happiness is.\”\n- Tosaka Rin, Unlimited Blade Works\n```");
 	}
 	
 });
